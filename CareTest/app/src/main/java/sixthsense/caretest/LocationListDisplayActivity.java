e5f4ccdc242e029b375242d77e36b1337d4f9c85
shrjai <shrjai@microsoft.com>
2017-03-08 01:34:50 +0530

    Init Commit 2

---
+++ CareTest/app/src/main/java/sixthsense/caretest/LocationListDisplayActivity.java
@@ -0,0 +1,15 @@
+package sixthsense.caretest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LocationListDisplayActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location_display);
+    }
+}
